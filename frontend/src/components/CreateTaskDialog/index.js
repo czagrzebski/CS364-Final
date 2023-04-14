aa7ac5fb3305c8ddc850f1727f63c068f7b18a4f
Creed Zagrzbebski <czagrzebski@DESKTOP-4JOT5NT>
2023-04-13 23:02:00 -0500

    Worked on task table

--- frontend/src/components/CreateTaskDialog/index.js
+++ frontend/src/components/CreateTaskDialog/index.js
@@ -3,7 +3,7 @@ import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import { TextField } from '@mui/material';
+import { TextField, Box, Select, MenuItem } from '@mui/material';
 import DialogTitle from '@mui/material/DialogTitle';
 
 export default function CreateTaskDialog({isCreateDialogOpen, setIsCreateDialogOpen, onTaskUpdate}) {
@@ -21,13 +21,41 @@ export default function CreateTaskDialog({isCreateDialogOpen, setIsCreateDialogO
       <Dialog open={isCreateDialogOpen} onClose={handleClose}>
         <DialogTitle>Add Task</DialogTitle>
         <DialogContent>
+          <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+          >
           <TextField
             required
             id="outlined-required"
-            label="Task Title"
+            label="Title"
             defaultValue=""
             color="info"
           />
+          <TextField
+          id="outlined-multiline-static"
+          label="Description"
+          multiline
+          rows={4}
+          color="info"
+          />
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={taskProject}
+            label="Project"
+            onChange={(event) => setTaskProject(event.target.value)}
+            color="info"
+          >
+            <MenuItem value={10}>Project 1</MenuItem>
+            <MenuItem value={20}>Project 2</MenuItem>
+            <MenuItem value={30}>Project 3</MenuItem>
+          </Select>
+          </Box>
         </DialogContent>
         <DialogActions>
           <Button color="info" onClick={handleClose}>Cancel</Button>
